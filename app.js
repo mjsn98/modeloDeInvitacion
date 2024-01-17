996cfdf8466cfadea204c5c172b7f8a049c5258a
mjsn98 <mjsn98@gmail.com>
2024-01-17 19:55:37 -0300

    change music

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port=process.env.PORT || 3000;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.listen(port,'0.0.0.0',()=>console.log(`Servidor corriendo en el puerto ${port}`)); 
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`)); 
 app.get('/',function(req,res){ 
     res.sendFile(path.join(__dirname,'views/index.html')); 
 });
