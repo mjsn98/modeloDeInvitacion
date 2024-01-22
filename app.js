9b4d3bb114a119ffcf06c916b69ea2cb6b7291fd
mjsn98 <mjsn98@gmail.com>
2024-01-22 00:51:02 -0300

    :)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const path=require('path');
 const port=process.env.PORT || 3000;
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`)); 
 app.get('/',function(req,res){ 
